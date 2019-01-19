07985438bbb83833b34d68a9e427e29914c9bd9d
MafCM <mf_b_j_w_mc@hotmail.com>
2019-01-19 10:12:53 -0600

    Proyecto final

---
+++ JS.js
@@ -0,0 +1,65 @@
+
+
+
+
+$(document).ready( ( ) => {
+
+
+
+alert("Proyecto integrador - ICONOS")
+
+
+
+
+
+imgLiquid()
+
+
+
+} )
+
+
+
+function imgLiquid() {
+
+    $(".imgLiquid.imgLiquidFill").imgLiquid()
+
+$(".imgLiquid.imgLiquidNoFill").imgLiquid({
+
+    fill:false
+
+})
+
+
+
+$(".imgLiquid.imgLiquidNoFillLeft").imgLiquid({
+
+    fill:false,
+
+    horizontalAling: "left"
+
+})
+
+
+
+$(".imgLiquid.imgLiquidNoFillRight").imgLiquid({
+
+    fill:false,
+
+    horizontalAling: "right"
+
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
